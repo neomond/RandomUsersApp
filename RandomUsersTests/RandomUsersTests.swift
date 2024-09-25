8099b73109f3449ea42c60c968a3fdefd156e372
neomond <neoweise@yahoo.com>
2024-09-25 17:22:10 +0400

    feat: created unit tests to verify that  decoding process works correctly

---
+++ RandomUsersTests/RandomUsersTests.swift
@@ -0,0 +1,32 @@
+//
+//  RandomUsersTests.swift
+//  RandomUsersTests
+//
+//  Created by Nazrin Atayeva on 25.09.24.
+//
+
+import XCTest
+@testable import RandomUsers
+
+final class RandomUsersTests: XCTestCase {
+    
+    func testUserModel() throws {
+        let jsonData = getTestJSONData()
+        
+        do {
+            _ = try JSONDecoder().decode(Response.self, from: jsonData)
+        } catch {
+           XCTFail("Failed to decode JSON into the model: \(error)")
+        }
+    }
+    
+
+    private func getTestJSONData() -> Data {
+        guard let path = Bundle.main.path(forResource: "randomUsers", ofType: "json") else {
+            fatalError("randomUsers.json file not found")
+        }
+        let internalURL = URL(fileURLWithPath: path)
+        return try! Data(contentsOf: internalURL)
+    }
+
+}
